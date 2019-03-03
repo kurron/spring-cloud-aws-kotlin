7ceb7432f97c06867b7c0577e2803e694343f8de
Ron Kurr <rkurr@overstock.com>
2019-03-03 19:00:44 +0000

    Starting to trim out unwanted code.

--- src/main/kotlin/org/kurron/spring/cloud/aws/Application.kt
+++ src/main/kotlin/org/kurron/spring/cloud/aws/Application.kt
@@ -14,26 +14,6 @@ class BlogApplication {
     @Bean
     fun mustacheCompiler( loader: Mustache.TemplateLoader? ) = Mustache.compiler().escapeHTML( false ).withLoader( loader )!!
 
-    @Bean
-    fun databaseInitializer( userRepository: UserRepository, articleRepository: ArticleRepository ) = CommandLineRunner {
-        val smaldini = User("smaldini", "Stéphane", "Maldini")
-        userRepository.save(smaldini)
-
-        articleRepository.save(Article(
-                "Reactor Bismuth is out",
-                "Lorem ipsum",
-                "dolor **sit** amet https://projectreactor.io/",
-                smaldini,
-                1
-        ))
-        articleRepository.save(Article(
-                "Reactor Aluminium has landed",
-                "Lorem ipsum",
-                "dolor **sit** amet https://projectreactor.io/",
-                smaldini,
-                2
-        ))
-    }
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/org/kurron/spring/cloud/aws/Repositories.kt
+++
@@ -1,9 +0,0 @@
-package org.kurron.spring.cloud.aws
-
-import org.springframework.data.repository.CrudRepository
-
-interface ArticleRepository : CrudRepository<Article, Long> {
-    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
-}
-
-interface UserRepository : CrudRepository<User, String>
