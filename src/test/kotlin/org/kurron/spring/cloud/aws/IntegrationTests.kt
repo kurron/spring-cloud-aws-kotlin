6dcfe5c5090ae2aaadfa419e86aaa33e99efd673
Ron Kurr <rkurr@overstock.com>
2019-03-03 18:45:36 +0000

    Starting to trim out unwanted code.

--- src/test/kotlin/org/kurron/spring/cloud/aws/IntegrationTests.kt
+++ src/test/kotlin/org/kurron/spring/cloud/aws/IntegrationTests.kt
@@ -32,15 +32,5 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         println(">> Assert org.kurron.spring.cloud.aws page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
-    }
-
-    @Test
-    fun `Assert article page title, content and status code`() {
-        println(">> Assert article page title, content and status code")
-        val entity = restTemplate.getForEntity<String>("/article/2")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("Reactor Aluminium has landed",
-                                         "<a href=\"https://projectreactor.io/\">https://projectreactor.io/</a>")
     }
 }
